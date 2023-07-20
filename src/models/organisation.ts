7ff28f0fec17340af82fa7a2c6c1697d893fe71d
Samuel <samueltwumasi1290.com>
2023-07-20 18:41:55 +0000

    update

--- src/models/organisation.ts
+++ src/models/organisation.ts
@@ -1,2 +1,22 @@
-import { Schema } from "mongoose";
+import { Schema, model } from "mongoose";
 
+const organisation_schema = new Schema<IOrganisation>({
+    name: {
+        type: String,
+        required: true,
+    },
+    projects: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Project',
+        },
+    ],
+    members: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    ],
+}, {versionKey: false},);
+
+export const Organisation = model('Organisation', organisation_schema);
